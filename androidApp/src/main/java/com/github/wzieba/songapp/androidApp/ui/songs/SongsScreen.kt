e371266d2470eeb176c3ac3bab003e5de3fa479a
AdamGrzybkowski <agrzybkowski@outlook.com>
2020-10-16 15:56:07 +0200

    feat: basic view with Jetpack Compose

---
+++ androidApp/src/main/java/com/github/wzieba/songapp/androidApp/ui/songs/SongsScreen.kt
@@ -0,0 +1,31 @@
+package com.github.wzieba.songapp.androidApp.ui.songs
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumnForIndexed
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.asLiveData
+import com.github.wzieba.songapp.androidApp.ui.songs.component.SongItem
+import com.github.wzieba.songapp.domain.ObserveSongs
+import com.github.wzieba.songapp.domain.Song
+
+@Composable
+fun SongsScreen(
+    observeSongs: ObserveSongs,
+) {
+    val songs: List<Song> by observeSongs.invoke().asLiveData().observeAsState(emptyList())
+    LazyColumnForIndexed(items = songs, contentPadding = PaddingValues(8.dp)) { position, song ->
+        Column {
+            SongItem(song = song)
+            if (position < songs.size) {
+                Spacer(Modifier.height(8.dp))
+            }
+        }
+    }
+}
---
+++ androidApp/src/main/java/com/github/wzieba/songapp/androidApp/ui/songs/component/SongItem.kt
@@ -0,0 +1,46 @@
+package com.github.wzieba.songapp.androidApp.ui.songs.component
+
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.ui.tooling.preview.Preview
+import com.github.wzieba.songapp.androidApp.ui.theme.SongAppTheme
+import com.github.wzieba.songapp.domain.Song
+
+@Composable
+fun SongItem(modifier: Modifier = Modifier, song: Song) {
+    Card {
+        Column(
+            modifier = modifier.fillMaxWidth()
+                .wrapContentHeight()
+                .padding(8.dp)
+        ) {
+            Text(text = song.artistName, style = MaterialTheme.typography.h5)
+            Text(text = song.name, style = MaterialTheme.typography.body2)
+            Text(text = song.releaseYear, style = MaterialTheme.typography.caption)
+        }
+    }
+}
+
+@Preview("SongItem light")
+@Composable
+fun previewSongItem() {
+    SongAppTheme(darkTheme = false) {
+        SongItem(song = Song("Song name", "Artist name", "1999"))
+    }
+}
+
+@Preview("SongItem dark")
+@Composable
+fun previewDarkSongItem() {
+    SongAppTheme(darkTheme = true) {
+        SongItem(song = Song("Song name", "Artist name", "1999"))
+    }
+}
