f346e61988b97a6fa6dd06c6f9bff78df4eea069
Wojciech Zieba <wojciech.zieba@protonmail.com>
2021-01-08 09:23:06 +0100

    build: update AGP and Gradle wrapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
-        classpath("com.android.tools.build:gradle:4.2.0-alpha14")
+        classpath("com.android.tools.build:gradle:4.2.0-beta03")
     }
 }
 group = "com.github.wzieba.songapp"
